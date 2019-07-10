727507dedf6e5955bdc855b2348b001c61ea4ad7
miaklwalker <ouisihai@gmail.com>
2019-07-10 02:58:38 -0500

    adding camera

--- dist/components/classes/systems/camera.js
+++ dist/components/classes/systems/camera.js
@@ -3,11 +3,20 @@ import loadImage from "../../functions/getImage.js";
 export default class camera {
     constructor() {
         this.position = new Vector(1, 1);
-        this.location = new Vector(1, 1);
+        this.location = new Vector(8, 8);
     }
     show(game, context) {
-        loadImage(game.json.overworld);
+        loadImage(game.json.urls.overworld)
+            .then(data => {
+            context.drawImage(data, 1800, 1228, 240, 240, 0, 120, game.width, game.height);
+        });
         context.fillStyle = 'saddleBrown';
         context.fillRect(0, 60, game.width, game.height);
     }
 }
+// 16 W    8 H  
+// 4096    1408
+// 157.5    176
+//  8       8
+// first Screen 
+//1800 1228
--- src/components/classes/systems/camera.ts
+++ src/components/classes/systems/camera.ts
@@ -4,13 +4,26 @@ import Game from "./game";
 
 export default class camera {
     position: Vector;
+    location: Vector;
     constructor(){
         this.position = new Vector(1,1);
-        this.location = new Vector(1,1);
+        this.location = new Vector(8,8);
     }
     show(game:Game,context:CanvasRenderingContext2D){
-        loadImage(game.json.overworld)
+        loadImage(game.json.urls.overworld)
+        .then(data =>{
+            context.drawImage(data,1800,1228,240,240,0,120,game.width,game.height)
+        })
         context.fillStyle='saddleBrown'
         context.fillRect(0,60,game.width,game.height)
     }
 }
+
+// 16 W    8 H  
+// 4096    1408
+// 157.5    176
+//  8       8
+
+
+// first Screen 
+//1800 1228
