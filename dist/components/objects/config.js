3fae51520646a8e930b6a23c58e1212357fdbedc
Kevin Bigler <kevinbigler@gmail.com>
2019-07-28 01:57:12 +0100

    Deleted dist contents, playing with tsconfig

--- dist/components/objects/config.js
+++
@@ -1,4 +0,0 @@
-import controlsConfig from "../classes/systems/controlsConfig.js";
-let config = new controlsConfig('ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'Enter', 'Space', 'KeyA', 'KeyB');
-export default config;
-//# sourceMappingURL=config.js.map
